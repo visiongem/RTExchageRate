6d203031aeab43df5b91d969e0598055dffb60f9
pengyn <393507488@qq.com>
2017-02-22 14:39:20 +0800

    commit

---
+++ app/src/main/java/com/pyn/rtexchagerate/base/SingleFragmentActivity.java
@@ -0,0 +1,36 @@
+package com.pyn.rtexchagerate.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.pyn.rtexchagerate.R;
+
+/**
+ * describtion:
+ * Created by pengyn on 2017/1/5.
+ */
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+
+        initDatas();
+    }
+
+    protected abstract void initDatas();
+}
